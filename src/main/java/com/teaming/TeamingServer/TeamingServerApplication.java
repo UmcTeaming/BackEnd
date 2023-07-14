26415a1c969d78e96d5c6933c517cbe5f8d68589
jay_010304 <glp0509@naver.com>
2023-07-14 13:40:30 +0900

    first commit

---
+++ src/main/java/com/teaming/TeamingServer/TeamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.teaming.TeamingServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teaming/TeamingServer/TeamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teaming.TeamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
