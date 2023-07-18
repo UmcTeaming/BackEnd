5301055f2f684752990a252693c61d1744eedb69
5nam <ohnam90@gmail.com>
2023-07-18 14:09:52 +0900

    [fix] : BaseResponse 상황별 생성자 추가

--- src/main/java/com/teaming/TeamingServer/common/BaseResponse.java
+++ src/main/java/com/teaming/TeamingServer/common/BaseResponse.java
@@ -6,12 +6,25 @@ import lombok.Getter;
 public class BaseResponse<T> {
 
     private final int status;
-    private final String message;
-    private final T data;
+    private String message; // 응답에 메시지가 포함 안될 수도 있으므로 final 뺌
+    private T data; // 응답에 데이터가 포함 안될 수도 있으므로 final 뺌
 
+    // status, message, data 모두 넘겨주는 Response
     public BaseResponse(int status, String message, T data) {
         this.status = status;
         this.message = message;
+        this.data = data;
+    }
+
+    // status, message 만 넘겨주는 Response
+    public BaseResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    // status, data 만 넘겨주는 Response
+    public BaseResponse(int status, T data) {
+        this.status = status;
         this.data = data;
     }
 }
