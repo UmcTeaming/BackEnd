b3744506d9d0c98303afc7bd14f3b06ca195a16d
5nam <ohnam90@gmail.com>
2023-07-19 23:29:12 +0900

    [feat] : MemberRepository 생성

--- src/main/java/com/teaming/TeamingServer/Repository/MemberRepository.java
+++ src/main/java/com/teaming/TeamingServer/Repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.teaming.TeamingServer.Repository;
 
+import com.teaming.TeamingServer.Domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Integer> {
 }
