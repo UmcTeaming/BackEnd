00096a8314a4ba692d665ed9a882888d9d7558af
5nam <ohnam90@gmail.com>
2023-07-19 23:27:24 +0900

    [feat] : MemberController 코드 추가

--- src/main/java/com/teaming/TeamingServer/Controller/MemberController.java
+++ src/main/java/com/teaming/TeamingServer/Controller/MemberController.java
@@ -1,6 +1,15 @@
 package com.teaming.TeamingServer.Controller;
 
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller            // 해당 클래스가 컨트롤러임을 알리고 bean으로 등록하기 위함
+@RequiredArgsConstructor
 public class MemberController {
+    @GetMapping("/")
+    public String Home() {
+        return "home";
+    }
 }
