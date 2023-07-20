23c983955d4127a48b859ff3011d77dda2e20bcb
5nam <ohnam90@gmail.com>
2023-07-20 15:07:30 +0900

    [feat] : MemberRequestDto 에 속성 추가

--- src/main/java/com/teaming/TeamingServer/Domain/Dto/MemberRequestDto.java
+++ src/main/java/com/teaming/TeamingServer/Domain/Dto/MemberRequestDto.java
@@ -1,15 +1,29 @@
 package com.teaming.TeamingServer.Domain.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberRequestDto {
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+//    @Length(min=8, max=16, message="비밀번호는 8자 이상, 16자 이하로 입력해주세요")
     private String password;
+
+    @NotEmpty(message = "위에 입력한 비밀번호와 일치하게 입력해주세요.")
     private String checkPassword;
 }
