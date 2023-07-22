2175eb8287e4b14ee38852c5fe427817c622eaa1
5nam <ohnam90@gmail.com>
2023-07-22 13:56:23 +0900

    [feat] : 회원가입과 이메일 중복체크 기능 추가 및 json 형식으로 반환해주는 ResponseBody 어노테이션 추가

--- src/main/java/com/teaming/TeamingServer/controller/MemberController.java
+++ src/main/java/com/teaming/TeamingServer/controller/MemberController.java
@@ -1,15 +1,16 @@
-package com.teaming.TeamingServer.Controller;
+package com.teaming.TeamingServer.controller;
 
 
 import com.teaming.TeamingServer.Domain.Dto.MemberRequestDto;
+import com.teaming.TeamingServer.Domain.entity.Member;
 import com.teaming.TeamingServer.Service.MemberService;
-import com.teaming.TeamingServer.common.BaseErrorResponse;
 import com.teaming.TeamingServer.common.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller            // 해당 클래스가 컨트롤러임을 알리고 bean으로 등록하기 위함
 @RequiredArgsConstructor
@@ -19,14 +20,24 @@ public class MemberController {
 
     // 회원가입
     @PostMapping("/signup")
+    @ResponseBody // json 으로 반환해주는 어노테이션
     public BaseResponse signup(@RequestBody MemberRequestDto memberRequestDto) {
-        memberService.join(memberRequestDto);
+        Member member = Member.builder()
+                .name(memberRequestDto.getName())
+                .email(memberRequestDto.getEmail())
+                .password(memberRequestDto.getPassword())
+                .agreement(true).build();
+
+        // String checkPassword = memberRequestDto.getCheckPassword();
+        memberService.join(member);
+
         // 여기서 Exception 을 잡아야 하나?
         return new BaseResponse(200, "회원가입이 완료되었습니다.");
     }
 
     // 이메일 중복체크
     @GetMapping("/signup/email-duplication")
+    @ResponseBody // json 으로 반환해주는 어노테이션
     public BaseResponse duplicateEmail(@RequestBody MemberRequestDto memberRequestDto) {
         try {
             memberService.validateDuplicateMember(memberRequestDto.getEmail());
