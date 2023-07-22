4e103aba8f563ecb3f0126fe487c9e0bc0a48e61
5nam <ohnam90@gmail.com>
2023-07-22 13:59:34 +0900

    [test] : MemberService 회원가입, 이메일 중복체크 테스트 코드 작성

--- src/test/java/com/teaming/TeamingServer/service/MemberServiceImplTest.java
+++ src/test/java/com/teaming/TeamingServer/service/MemberServiceImplTest.java
@@ -1,2 +1,68 @@
-package com.teaming.TeamingServer.service;public class MemberServiceImplTest {
+package com.teaming.TeamingServer.service;
+
+import com.teaming.TeamingServer.Domain.entity.Member;
+import com.teaming.TeamingServer.Repository.MemberRepository;
+import com.teaming.TeamingServer.Service.MemberServiceImpl;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.jayway.jsonpath.internal.path.PathCompiler.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceImplTest {
+
+    @Autowired MemberServiceImpl memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    public void 회원가입() throws Exception {
+
+        //given
+        Member member = Member.builder()
+                .name("홍길동")
+                .email("test@gmail.com")
+                .password("test1234")
+                .agreement(true)
+                .build();
+
+        //when
+        Integer savedId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findById(savedId).get());
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+        Member member1 = Member.builder()
+                .name("홍길동")
+                .email("test@gmail.com")
+                .password("test1234")
+                .agreement(true)
+                .build();
+
+        Member member2 = Member.builder()
+                .name("홍길동")
+                .email("test@gmail.com")
+                .password("test1234")
+                .agreement(true)
+                .build();
+
+        //when
+        memberService.join(member1);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            memberService.join(member2);
+        }); //예외가 발생해야 한다!!!
+    }
+
 }
