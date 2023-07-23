45fe82b9aca53944884c1c7bcc4dd90b5da0affb
5nam <ohnam90@gmail.com>
2023-07-23 10:54:07 +0900

    [refactor] : 공백 입력 허용 안되도록 NotBlank 어노테이션으로 변경

--- src/main/java/com/teaming/TeamingServer/Domain/Dto/MemberRequestDto.java
+++ src/main/java/com/teaming/TeamingServer/Domain/Dto/MemberRequestDto.java
@@ -13,17 +13,17 @@ import org.hibernate.validator.constraints.Length;
 @AllArgsConstructor
 public class MemberRequestDto {
 
-    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @NotBlank(message = "이름은 필수 입력 값입니다.(공백 없이 입력해주세요.)")
     private String name;
 
-    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.(공백 없이 입력해주세요.)")
     @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
-    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.(공백 없이 입력해주세요.)")
 //    @Length(min=8, max=16, message="비밀번호는 8자 이상, 16자 이하로 입력해주세요")
     private String password;
 
-    @NotEmpty(message = "위에 입력한 비밀번호와 일치하게 입력해주세요.")
+    @NotBlank(message = "위에 입력한 비밀번호와 일치하게 입력해주세요.(공백 없이 입력해주세요.)")
     private String checkPassword;
 }
