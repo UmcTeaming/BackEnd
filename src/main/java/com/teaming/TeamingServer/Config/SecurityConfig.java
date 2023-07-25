3f8d62b66465062a03e22c1118fc0b97d9191138
5nam <ohnam90@gmail.com>
2023-07-25 17:41:40 +0900

    [feat] : login 기능 구현을 위한 Request, Response, gradle 추가

--- src/main/java/com/teaming/TeamingServer/Config/SecurityConfig.java
+++ src/main/java/com/teaming/TeamingServer/Config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.teaming.TeamingServer.Config;
 
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
