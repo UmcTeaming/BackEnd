8123e1dabfd8646195f66ebae01fe9414e65bdff
5nam <ohnam90@gmail.com>
2023-08-02 01:39:00 +0900

    [feat] : 로그인 접근 권한 Security Config 에 적용 및 Role 없음에 오류 발생하여, Role 강제 부여

--- src/main/java/com/teaming/TeamingServer/Config/SecurityConfig.java
+++ src/main/java/com/teaming/TeamingServer/Config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.teaming.TeamingServer.Config;
 
-import com.teaming.TeamingServer.Config.Jwt.CustomMemberDetailsService;
 import com.teaming.TeamingServer.Config.Jwt.JwtAuthenticationFilter;
 import com.teaming.TeamingServer.Config.Jwt.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +18,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+
+    private String[] possibleAccess = {"/auth/signup"
+            , "/auth/email-duplication", "/auth/email-verification", "/auth/login"
+            , "/auth/reset-password", "/member/reissue", "/h2-console/*", "/error"};
 
     public SecurityConfig(JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
@@ -36,17 +37,20 @@ public class SecurityConfig {
         // h2 접근 허용
         http
                 .headers((header) -> header.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable()));
+        http
+                .csrf((csrf) -> csrf.ignoringRequestMatchers("/h2-console/**").disable());
 
         http
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement((sessionManagement) ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.anyRequest().permitAll());
-                // .formLogin((formLogin) -> formLogin.loginProcessingUrl("/login"))
-                // .authorizeHttpRequests((antMatchers) -> antMatchers.requestMatchers("/member/").permitAll())
+                .authorizeHttpRequests((authorizeRequests) -> authorizeRequests.requestMatchers(possibleAccess).permitAll()
+                        .anyRequest().authenticated());
+        // .formLogin((formLogin) -> formLogin.loginProcessingUrl("/login"))
         http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
